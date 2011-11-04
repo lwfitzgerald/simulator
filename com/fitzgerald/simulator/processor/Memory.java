42d847b457d4011746fe902f8f38a8117938c986
Luke Fitzgerald <lw.fitzgerald@gmail.com>
2011-11-04 13:50:23 +0000

    Removed program code bits from Memory class and switched from Integers being stored to byte[]

--- com/fitzgerald/simulator/processor/Memory.java
+++ com/fitzgerald/simulator/processor/Memory.java
@@ -1,56 +1,14 @@
 package com.fitzgerald.simulator.processor;
 
-import java.io.FileInputStream;
-import java.io.ObjectInputStream;
 import java.util.HashMap;
 import java.util.Random;
-
-import com.fitzgerald.simulator.instruction.Instruction;
 
 public class Memory {
     
     /**
-     * Program "loaded" into memory
-     */
-    protected Program program;
-    
-    /**
      * HashMap mapping memory address to value
      */
-    protected HashMap<Integer, Integer> memorySpace = new HashMap<Integer, Integer>();
-    
-    /**
-     * Initialises memory with the program from
-     * the given program
-     * @param programPath Path to program file
-     */
-    public Memory(String programPath) throws Exception {
-        loadProgram(programPath);
-    }
-    
-    protected void loadProgram(String programPath) throws Exception {
-        FileInputStream inputStream = new FileInputStream(programPath);
-        ObjectInputStream objectStream = new ObjectInputStream(inputStream);
-        
-        program = (Program) objectStream.readObject();
-    }
-    
-    /**
-     * Get instruction at memory address
-     * @param memoryAddress
-     * @return
-     */
-    public Instruction getInstruction(int memoryAddress) {
-        if (memoryAddress >= program.getEndLocation()) {
-            /*
-             * If we're outside the memory area of the program
-             * return null
-             */
-            return null;
-        }
-        
-        return program.getInstruction(memoryAddress);
-    }
+    protected HashMap<Integer, byte[]> memorySpace = new HashMap<Integer, byte[]>();
     
     /**
      * Load from a memory address
@@ -58,13 +16,8 @@ public class Memory {
      * @return Word at address
      * @throws MemoryException
      */
-    public Integer load(int memoryAddress) throws MemoryException {
-        // Check we're not trying to read in the program memory area
-        if (memoryAddress < program.getEndLocation()) {
-            throw new MemoryException("Attempting to read from program memory area");
-        }
-        
-        Integer loaded;
+    public byte[] load(int memoryAddress) throws MemoryException {
+        byte[] loaded;
         
         if ((loaded = memorySpace.get(memoryAddress)) == null) {
             /*
@@ -75,18 +28,15 @@ public class Memory {
              * and returning that
              */
             Random generator = new Random();
-            loaded = generator.nextInt();
+            loaded = Util.intToBytes(generator.nextInt());
             memorySpace.put(memoryAddress, loaded);
         }
         
         return loaded;
     }
     
-    public void store(int memoryAddress) throws MemoryException {
-        // Check we're not trying to write in the program memory area
-        if (memoryAddress < program.getEndLocation()) {
-            throw new MemoryException("Attempting to write to program memory area");
-        }
+    public void store(int memoryAddress, byte[] toStore) throws MemoryException {
+        memorySpace.put(memoryAddress, toStore);
     }
     
 }
