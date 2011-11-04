558606a57cfd61ed1c76604ced8f92d273af29e4
Luke Fitzgerald <lw.fitzgerald@gmail.com>
2011-11-04 16:55:35 +0000

    Soft tabs!!!!!

--- com/fitzgerald/simulator/processor/Simulator.java
+++ com/fitzgerald/simulator/processor/Simulator.java
@@ -8,46 +8,46 @@ public class Simulator {
 
     protected Processor processor;
     
-	public static void main(String[] args) {
-	    /*RegisterFile registerFile = RegisterFile.getSingleton();
-	    Register intReg0 = registerFile.getRegister(0);
-	    intReg0.setCurrentValue(0);
-	    intReg0.setNextValue(5);
-	    intReg0.finishCycle();
-	    System.out.println(intReg0.getCurrentValue());*/
-	    
-	    test();
-	}
-	
+    public static void main(String[] args) {
+        /*RegisterFile registerFile = RegisterFile.getSingleton();
+        Register intReg0 = registerFile.getRegister(0);
+        intReg0.setCurrentValue(0);
+        intReg0.setNextValue(5);
+        intReg0.finishCycle();
+        System.out.println(intReg0.getCurrentValue());*/
+        
+        test();
+    }
+    
     public Simulator getSingleton() {
         return singleton;
     }
     
-	/**
-	 * Creates an instance of the simulator for the given
-	 * program file
-	 * @param programFile Program file to load and run
-	 */
-	public Simulator(String programFile) {
-	    
-	}
-	
-	public static void test() {
-	    System.out.println("Starting tests");
-	    
-	    Register.test();
-	    RegisterFile.test();
-	    
-	    LinkedList<String> failedTests = TestUtil.getFailedTests();
-	    
-	    if (failedTests.size() > 0) {
-	        System.err.println("Some tests failed:");
-	        
-	        for (String function : failedTests) {
-	        	System.err.println("Testing " + function + " FAILED");
-	        }
-	    } else {
-	        System.out.println("All tests passed :)");
-	    }
-	}
+    /**
+     * Creates an instance of the simulator for the given
+     * program file
+     * @param programFile Program file to load and run
+     */
+    public Simulator(String programFile) {
+        
+    }
+    
+    public static void test() {
+        System.out.println("Starting tests");
+        
+        Register.test();
+        RegisterFile.test();
+        
+        LinkedList<String> failedTests = TestUtil.getFailedTests();
+        
+        if (failedTests.size() > 0) {
+            System.err.println("Some tests failed:");
+            
+            for (String function : failedTests) {
+                System.err.println("Testing " + function + " FAILED");
+            }
+        } else {
+            System.out.println("All tests passed :)");
+        }
+    }
 }
