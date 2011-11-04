858554ca68eb9221f0f35fda964e706104f22a00
Luke Fitzgerald <lw.fitzgerald@gmail.com>
2011-11-04 00:48:31 +0000

    Adding placeholder Add and J classes

---
+++ com/fitzgerald/simulator/instruction/J.java
@@ -0,0 +1,17 @@
+package com.fitzgerald.simulator.instruction;
+
+public class J extends Instruction {
+
+    @Override
+    protected boolean conditional() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    protected void execute() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
