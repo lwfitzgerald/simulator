49449b4977e5138f251e790ece88ba293513c7d0
Luke Fitzgerald <lw.fitzgerald@gmail.com>
2011-11-07 16:36:51 +0000

    Add Sub instruction

---
+++ com/fitzgerald/simulator/instruction/Sub.java
@@ -0,0 +1,45 @@
+package com.fitzgerald.simulator.instruction;
+
+import com.fitzgerald.simulator.pipeline.DecodeStage;
+import com.fitzgerald.simulator.pipeline.ExecuteStage;
+import com.fitzgerald.simulator.processor.RegisterFile;
+import com.fitzgerald.simulator.processor.Util;
+
+public class Sub extends Instruction {
+
+    /**
+     * Serialisation ID
+     */
+    private static final long serialVersionUID = -12971856879705209L;
+
+    @Override
+    protected boolean conditional() {
+        // Always execute
+        return true;
+    }
+
+    @Override
+    public void decode(RegisterFile registerFile, DecodeStage decodeStage) {
+        byte[] sourceData1 = registerFile.getRegister(Util.bytesToInt(operand2)).getCurrentValue();
+        byte[] sourceData2 = registerFile.getRegister(Util.bytesToInt(operand3)).getCurrentValue();
+        
+        /*
+         * Do a deep copy of the source data and store the
+         * reference as the next source data value for this
+         * instruction
+         */
+        decodeStage.setSourceData1(sourceData1.clone());
+        decodeStage.setSourceData1(sourceData2.clone());
+    }
+
+    @Override
+    protected void executeOperation(RegisterFile registerFile, ExecuteStage executeStage) {
+        int srcInt1 = Util.bytesToInt(executeStage.getSourceData1());
+        int srcInt2 = Util.bytesToInt(executeStage.getSourceData2());
+        int result = srcInt1 - srcInt2;
+
+        // Set value as register's next value
+        registerFile.getRegister(Util.bytesToInt(operand1)).setNextValue(Util.intToBytes(result));
+    }
+
+}
