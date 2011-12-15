77d6bd584619610308567b3293c968f9abc890da
Luke Fitzgerald <lw.fitzgerald@gmail.com>
2011-12-15 23:24:35 +0000

    Add print statements to fibonacci

--- Simulator2/benchmarks/fibonacci.asm
+++ Simulator2/benchmarks/fibonacci.asm
@@ -1,9 +1,13 @@
+
+
         LDR r0, 0 # i-2
         LDR r1, 1 # i-1
         LDR r2, 10 # Loop counter
         LDR r3, 0 # 0 comparator
-        NOP
+        PRINT r0
+        PRINT r1
 Loop:   ADD r4, r1, r0 # Fi = Fi-1 + Fi-2
+        PRINT r4
         SUBI r2, r2, 1 # Loop counter--
         ADDI r0, r1, 0 # Fi-2 = Fi-1
         ADDI r1, r4, 0 # Fi-1 = Fi
